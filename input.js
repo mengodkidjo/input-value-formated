1f77a48dd9321a104369f8e4d86b9242418755b3
ledoc <71458682+mengodkidjo@users.noreply.github.com>
2022-09-08 20:33:05 +0200

    first commit

---
+++ input.js
@@ -0,0 +1,55 @@
+var maxLengthWanted = 16;
+var totalBeforSpace = 4;
+var separator = "-";
+
+const allInput = this.document.querySelectorAll(".param_input");
+allInput.forEach((input) => {
+    input.addEventListener("keypress",(e) => {
+        updateGlobalVar();
+
+})
+
+})
+var inputVar = this.document.getElementById("card_value");
+inputVar.maxLength = maxLengthWanted + ( maxLengthWanted / totalBeforSpace) - 1 ;
+inputVar.addEventListener("keypress", (e) => {
+    console.log(e);
+     updateGlobalVar();
+    if (e.key == separator) {
+        e.preventDefault()
+    } else {
+        if(inputVar.value.length  > 1 && e.keyCode != 8){
+                    inputVar.value = insertSpace(inputVar.value, totalBeforSpace, maxLengthWanted);
+                }   
+    }
+       
+        
+})
+
+function insertSpace(value, charNumber, maxLength){
+
+    if(value.length == charNumber){
+        return value + separator; 
+    } else if(value.length < charNumber){
+        return value.trim();
+    } else if(value.length <= maxLength && value.replaceAll(separator, "").length % charNumber == 0){
+        return value + separator    ;
+    } else if(value.replaceAll(separator, "").length > maxLength){
+        return value.substring(0, maxLength );
+    } else {
+        return value;
+    }
+}
+
+function updateGlobalVar() {
+   
+    separator = this.document.getElementById("separator").value == ""? " " : this.document.getElementById("separator").value;
+    maxLengthWanted = this.document.getElementById("max_input_lenght").value == "" ? 16 : parseInt(this.document.getElementById("max_input_lenght").value);
+    totalBeforSpace = this.document.getElementById("before_space").value == "" ? 4 : parseInt(this.document.getElementById("before_space").value);
+    if (maxLengthWanted < totalBeforSpace) {
+        alert("Seriously ?! what do you want ?");
+        this.document.getElementById("before_space").value = "";
+        this.document.getElementById("max_input_lenght").value = "";
+     }
+
+}
